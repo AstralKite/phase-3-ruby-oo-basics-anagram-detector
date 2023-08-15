19e03944f381ede483b8dcb0d98d2bfff9205485
Fausto Ramirez <ramirezmedia@gmail.com>
2023-08-14 20:11:23 -0400

    commit tree 02326276d88a01bda809fec04c26a6fab16f8d7e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+
+        matched = []
+
+        matched = arr.filter do |b|
+            word.split("").sort == b.split("").sort
+        end
+
+        puts matched;
+        matched
+    end
+end
